0fd5b0c2ed0bb77e406b6478425840edf0eb15e9
mymrtt <mymrtt@gmail.com>
2020-05-06 10:44:06 -0300

    Updates

---
+++ precache-manifest.61f5e70fbc8bd86521fdc2cac62a333b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21c9ecdbae3c601d2aedf6e1604c416f",
+    "url": "/uniaorio/index.html"
+  },
+  {
+    "revision": "eac898c9e9a806462a8b",
+    "url": "/uniaorio/static/css/main.de378a80.chunk.css"
+  },
+  {
+    "revision": "13b27a5779fb2eb66e47",
+    "url": "/uniaorio/static/js/2.961753e3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/uniaorio/static/js/2.961753e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eac898c9e9a806462a8b",
+    "url": "/uniaorio/static/js/main.633456d7.chunk.js"
+  },
+  {
+    "revision": "ecd5d8f3fadd1f84f3ee",
+    "url": "/uniaorio/static/js/runtime-main.cb743201.js"
+  },
+  {
+    "revision": "32d24ad489042de6ab86546db7281daf",
+    "url": "/uniaorio/static/media/check.32d24ad4.svg"
+  },
+  {
+    "revision": "22c1614329ab52138ba006c773dcb1c4",
+    "url": "/uniaorio/static/media/densDiagonal.22c16143.svg"
+  },
+  {
+    "revision": "23396a5d8a5d08631692a628d61fb628",
+    "url": "/uniaorio/static/media/densHorizontal.23396a5d.svg"
+  },
+  {
+    "revision": "1cac0dbe0e972d77d38280f29e128abc",
+    "url": "/uniaorio/static/media/densVertical.1cac0dbe.svg"
+  },
+  {
+    "revision": "a971af966d27c3fefd5b64836a2c1314",
+    "url": "/uniaorio/static/media/dropUp.a971af96.svg"
+  },
+  {
+    "revision": "4603df692bc130e094f0e145b1198240",
+    "url": "/uniaorio/static/media/dropdownBlack.4603df69.svg"
+  },
+  {
+    "revision": "1f90e964425b6ab752c2b1fba515fa5d",
+    "url": "/uniaorio/static/media/dropdownWhite.1f90e964.svg"
+  },
+  {
+    "revision": "0016974744b6825428497f9857b240b4",
+    "url": "/uniaorio/static/media/loading.00169747.svg"
+  },
+  {
+    "revision": "3f9b12715a33d92a9a71cc6180eebf73",
+    "url": "/uniaorio/static/media/logo.3f9b1271.png"
+  },
+  {
+    "revision": "96058ee8753e766bb6721dd48f444a12",
+    "url": "/uniaorio/static/media/mask.96058ee8.svg"
+  }
+]);
